6be7cbb5195eda272042cc6fb8a03ef40a9c4882
Seldszar <0xfuture@gmail.com>
2014-06-13 11:19:20 +0200

    Version incrementation update Refactoring Gruntfile.js Deletion of extension archives

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,138 +1,117 @@
 var fs = require('fs'),
-  cp = require('child_process'),
-  exec = cp.exec;
+  semver = require('semver');
 
-module.exports = function (grunt){
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    v                  : grunt.file.readJSON('version.json'),
-    clean              : {
-      dist   : {
-        src: ["./dist/*"]
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: {
+        src: ['dist/*']
       },
-      opera  : {
-        src: ["./build/opera/*"]
+      opera: {
+        src: ['build/opera/*']
       },
-      chrome : {
-        src: ["./build/chrome/*"]
+      chrome: {
+        src: ['build/chrome/*']
       },
       firefox: {
-        src: ["./build/firefox/*"]
+        src: ['build/firefox/*']
       }
     },
-    watch              : {
-      firefox: {
-        files  : ['templates/**', 'common/**', "firefox/**", "chrome/**", "opera/**"],
-        tasks  : ['firefox', 'chrome', 'opera'],
+    watch: {
+      dist: {
+        files: ['templates/**', 'common/**', 'firefox/**', 'chrome/**', 'opera/**'],
+        tasks: ['firefox', 'chrome', 'opera'],
         options: {
           nospawn: true
         }
       }
     },
-    version            : {
-      options  : {
-        pkg   : "version.json",
-        prefix: '[\'"]version[\'"]?\\s*[:=]\\s*[\'"]'
-      },
+    version: {
       manifests: {
         src: ['chrome/manifest.json', 'firefox/package.json', 'opera/manifest.json']
       }
     },
-//    concat    : {
-//      options: {
-//        separator: ';\n\n'
-//      },
-//        src : chromePopup,
-//        dest: "build/chrome/common/lib/concat.js"
-//      }
-//    },
-    copy               : {
+    copy: {
       firefox: {
-        files: [
-          {
-            expand: true,
-            src   : ["oauth2.js"],
-            cwd   : "./common/lib",
-            dest  : './build/firefox/lib'
-          },
-          {
-            expand: true,
-            src   : ["./common/**"],
-            dest  : './build/firefox/data'
-          },
-          {
-            expand: true,
-            src   : ["./firefox/**"],
-            cwd   : "./",
-            dest  : './build/'
-          }
-        ]
+        files: [{
+          expand: true,
+          src: ['oauth2.js'],
+          cwd: 'common/lib',
+          dest: 'build/firefox/lib'
+        }, {
+          expand: true,
+          src: ['common/**'],
+          dest: 'build/firefox/data'
+        }, {
+          expand: true,
+          src: ['firefox/**'],
+          dest: 'build/'
+        }]
       },
-      opera  : {
-        files: [
-          {
-            expand: true,
-            src   : ["**"],
-            cwd   : "./build/chrome",
-            dest  : './build/opera'
-          },
-          {
-            expand: true,
-            src   : ["./opera/**"],
-            cwd   : "./",
-            dest  : './build/'
-          }
-        ]
+      opera: {
+        files: [{
+          expand: true,
+          src: ['**'],
+          cwd: 'build/chrome',
+          dest: 'build/opera'
+        }, {
+          expand: true,
+          src: ['opera/**'],
+          dest: 'build/'
+        }]
       },
-      chrome : {
-        files: [
-          {
-            expand: true,
-            src   : ["./common/**", "./_locales/**"],
-            dest  : './build/chrome'
-          },
-          {
-            expand: true,
-            src   : ["./chrome/**"],
-            cwd   : "./",
-            dest  : './build/'
-          }
-        ]}
+      chrome: {
+        files: [{
+          expand: true,
+          src: ['common/**', '_locales/**'],
+          dest: 'build/chrome'
+        }, {
+          expand: true,
+          src: ['chrome/**'],
+          dest: 'build/'
+        }]
+      }
     },
-    handlebars         : {
+    handlebars: {
       compile: {
         options: {
-          namespace  : "Handlebars.templates",
-          wrapped    : true,
-          processName: function (filename){
-            return filename.split("/").pop();
+          namespace: 'Handlebars.templates',
+          wrapped: true,
+          processName: function (filename) {
+            return filename.split('/').pop();
           }
         },
 
         files: {
-          "common/dist/templates.js": "templates/*"
+          'common/dist/templates.js': 'templates/*'
         }
       }
     },
-    compress           : {
-      opera : {
+    compress: {
+      opera: {
         options: {
-          mode   : "zip",
-          archive: 'dist/twitch-now-opera-<%= v.version %>.crx'
+          mode: 'zip',
+          archive: 'dist/twitch-now-opera-<%= pkg.version %>.crx'
         },
-        files  : [
-          {src: ['**'], cwd: "build/opera/", expand: true }
-        ]
+        files: [{
+          src: ['**'],
+          cwd: 'build/opera/',
+          expand: true
+        }]
       },
       chrome: {
         options: {
-          mode   : "zip",
-          archive: 'dist/twitch-now-chrome-<%= v.version %>.zip'
+          mode: 'zip',
+          archive: 'dist/twitch-now-chrome-<%= pkg.version %>.zip'
         },
-        files  : [
-          {src: ['**'], cwd: "build/chrome/", expand: true }
-        ]
+        files: [{
+          src: ['**'],
+          cwd: 'build/chrome/',
+          expand: true
+        }]
       }
     },
     'mozilla-addon-sdk': {
@@ -142,51 +121,49 @@ module.exports = function (grunt){
         }
       }
     },
-    'mozilla-cfx'      : {
+    'mozilla-cfx': {
       'run_stable': {
         options: {
-          "mozilla-addon-sdk": "1_16",
-          extension_dir      : "build/firefox",
-          command            : "run"
+          'mozilla-addon-sdk': '1_16',
+          extension_dir: 'build/firefox',
+          command: 'run'
         }
       }
     },
-    'mozilla-cfx-xpi'  : {
+    'mozilla-cfx-xpi': {
       stable: {
         options: {
           'mozilla-addon-sdk': '1_16',
-          extension_dir      : 'build/firefox',
-          dist_dir           : 'dist/',
-          arguments          : '--output-file=twitch-now-firefox-<%= v.version %>.xpi'
+          extension_dir: 'build/firefox',
+          dist_dir: 'dist/',
+          arguments: '--output-file=twitch-now-firefox-<%= pkg.version %>.xpi'
+        }
+      }
+    },
+    gittag: {
+      bump: {
+        options: {
+          tag: '<%= pkg.version %>'
         }
       }
     }
   });
 
-  grunt.registerTask('i18n', function (){
-    var localesObj = {};
-    var locales = fs.readdirSync("_locales");
-    for ( var i = 0; i < locales.length; i++ ) {
-      var file = fs.readFileSync(__dirname + "/_locales/" + locales[i] + "/messages.json");
-      localesObj[locales[i]] = JSON.parse(file);
-    }
-    fs.writeFileSync("common/dist/locales.json", JSON.stringify(localesObj, null, 2), "utf8");
+  grunt.registerTask('i18n', function () {
+    var locales = {};
+    grunt.file.recurse('_locales', function (abspath, rootdir, subdir, filename) {
+      if (filename == 'messages.json') {
+        locales[subdir] = grunt.file.readJSON(abspath);
+      }
+    });
+    grunt.file.write('common/dist/locales.json', JSON.stringify(locales, null, 2));
   });
 
-  grunt.registerTask('bump', function (){
-    var done = this.async();
-    try {
-      var manifest = JSON.parse(fs.readFileSync("./version.json"));
-      var version = manifest.version.split(".");
-      var l = version.length;
-      version[l - 1] = parseInt(version[l - 1], 10) + 1;
-      manifest.version = version.join(".");
-      fs.writeFileSync("./version.json", JSON.stringify(manifest, null, 2));
-    } catch (e) {
-      grunt.log.error(e);
-      return false;
-    }
-    done();
+  grunt.registerTask('bump', function () {
+    var pkg = grunt.file.readJSON('package.json');
+    pkg.version = semver.inc(pkg.version, 'patch');
+    grunt.config.set('pkg', pkg);
+    grunt.file.write('package.json', JSON.stringify(pkg, null, 2));
   });
 
   grunt.loadNpmTasks('grunt-mozilla-addon-sdk');
@@ -197,11 +174,11 @@ module.exports = function (grunt){
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-version');
+  grunt.loadNpmTasks('grunt-git');
 
-
-  grunt.registerTask('default', 'chrome'.split(' '));
-  grunt.registerTask('opera', 'clean:opera handlebars copy:opera'.split(' '));
-  grunt.registerTask('firefox', 'clean:firefox i18n handlebars copy:firefox'.split(' '));
-  grunt.registerTask('chrome', 'clean:chrome handlebars copy:chrome'.split(' '));
-  grunt.registerTask('dist', 'clean:dist bump version chrome opera firefox compress:chrome compress:opera mozilla-addon-sdk mozilla-cfx-xpi'.split(' '));
+  grunt.registerTask('default', ['chrome']);
+  grunt.registerTask('opera', ['clean:opera', 'handlebars', 'copy:opera']);
+  grunt.registerTask('firefox', ['clean:firefox', 'i18n', 'handlebars', 'copy:firefox']);
+  grunt.registerTask('chrome', ['clean:chrome', 'handlebars', 'copy:chrome']);
+  grunt.registerTask('dist', ['clean:dist', 'bump', 'version', 'chrome', 'opera', 'firefox', 'compress:chrome', 'compress:opera', 'mozilla-addon-sdk', 'mozilla-cfx-xpi', 'gittag:bump']);
 };
