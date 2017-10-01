68b1b22ab9e23c088d43748ebd59cd3e29ec8462
ndragomirov <jdavid214@gmail.com>
2017-10-01 04:45:05 +0400

    restore old theater check

--- common/content/theatre-mode.js
+++ common/content/theatre-mode.js
@@ -1,10 +1,17 @@
 (function (){
 
   var theatreBtnSelector = '.js-control-theatre';
+  const MAX_TRIES = 400;
 
   function waitUntilVisible(selector, callback){
+    var tries = 0;
     var tm = setInterval(function (){
       var el = document.querySelector(selector);
+      if ( ++tries > MAX_TRIES ) {
+        clearInterval(tm);
+        callback("timeout error");
+      }
+
       if ( el && el.offsetHeight > 0 ) {
         clearInterval(tm);
         callback();
@@ -30,11 +37,13 @@
   }
 
   document.addEventListener("DOMContentLoaded", function (){
-    var hash = window.location.search;
-    var ref = document.referrer || "";
-    if ( /mode=theater/i.test(ref) ) {
-      waitUntilVisible(theatreBtnSelector, function (){
-        turnOn();
+    var hash = window.location.search; // no iframes
+    var ref = document.referrer || ""; //inside player's iframe
+    if ( /mode=theater/i.test(ref) || /mode=theater/i.test(hash) ) {
+      waitUntilVisible(theatreBtnSelector, function (err){
+        if ( !err ) {
+          turnOn();
+        }
       })
     }
   })
