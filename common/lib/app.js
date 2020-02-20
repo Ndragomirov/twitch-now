0f7b9ad16a09beda600963390cf24a38da40c66d
Mark <simpriani@gmail.com>
2020-02-20 16:25:09 +0200

    Allow disable split notifications

--- common/lib/app.js
+++ common/lib/app.js
@@ -430,6 +430,7 @@
       type: "select",
       select: true,
       opts: [
+        { id: "0", name: "0" },
         { id: "1", name: "1" },
         { id: "2", name: "2" },
         { id: "3", name: "3" },
