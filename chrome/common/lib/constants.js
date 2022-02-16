6630b69461187251430b50b4edfde2aee6f14a9f
Mark <simpriani@gmail.com>
2022-02-16 21:30:32 +0200

    Helix auth point & permissions

--- chrome/common/lib/constants.js
+++ chrome/common/lib/constants.js
@@ -4,11 +4,11 @@
 
   var constants = {
     twitchApi: {
-      api          : "https://api.twitch.tv/kraken/oauth2/authorize",
+      api          : "https://id.twitch.tv/oauth2/authorize",
       response_type: 'token',
       client_id    : 'b4sj5euottb8mm7pc1lfvi84kvzxqxk',
       client_secret: '2m42qpmxfy5l2ik4c93s0qco4vzfgr0',
-      scope        : 'user_follows_edit user_read',
+      scope        : 'user:read:follows',
       redirect_uri : 'http://ndragomirov.github.io/twitch.html'
     }
   };
