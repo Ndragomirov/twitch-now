1b0ec95587c80362f32cad3f9ec38f50137975d0
Mark <simpriani@gmail.com>
2022-02-17 14:41:25 +0200

    Fix sorting streams

--- common/lib/app.js
+++ common/lib/app.js
@@ -351,15 +351,15 @@
       type: "select",
       select: true,
       opts: [
-        { id: "viewers|1", name: "__MSG_m10__" },
-        { id: "viewers|-1", name: "__MSG_m11__" },
+        { id: "viewer_count|1", name: "__MSG_m10__" },
+        { id: "viewer_count|-1", name: "__MSG_m11__" },
         { id: "name|1", name: "__MSG_m12__" },
         { id: "name|-1", name: "__MSG_m13__" },
-        { id: "game|1", name: "__MSG_m82__" },
-        { id: "created_at|-1", name: "__MSG_m14__" }
+        { id: "game_name|1", name: "__MSG_m82__" },
+        { id: "started_at|-1", name: "__MSG_m14__" }
       ],
       show: true,
-      value: "viewers|-1"
+      value: "viewer_count|-1"
     },
     {
       id: "themeType",
