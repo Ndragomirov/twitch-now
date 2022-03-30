182f8835b614bd7095cc4b2c90175bc223443ae5
Mark <simpriani@gmail.com>
2022-03-30 17:24:35 +0300

    New client_id for chrome build

--- chrome/common/lib/constants.js
+++ chrome/common/lib/constants.js
@@ -6,10 +6,10 @@
     twitchApi: {
       api          : "https://id.twitch.tv/oauth2/authorize",
       response_type: 'token',
-      client_id    : 'b4sj5euottb8mm7pc1lfvi84kvzxqxk',
+      client_id    : 'wan7tdfzghu0uiisb8xoa1f4rgzsz6',
       client_secret: '2m42qpmxfy5l2ik4c93s0qco4vzfgr0',
       scope        : 'user:read:follows',
-      redirect_uri : 'http://ndragomirov.github.io/twitch.html'
+      redirect_uri : 'https://ndragomirov.github.io/twitch.html'
     }
   };
 
